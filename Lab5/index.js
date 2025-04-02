d00d85c90395639dbd9d09d33259fd8374deae07
mimahoney <mahoney.mich@northeastern.edu>
2025-04-01 21:46:20 -0400

    Update index.js

--- Lab5/index.js
+++ Lab5/index.js
@@ -9,6 +9,12 @@ export default function Lab5(app) {
   app.get("/lab5", (req, res) => {
     res.send("Welcome to Lab 5!");
   });  
+  app.get("/lab5/assignment", (req, res) => {
+    res.json([
+      { id: 1, title: "Assignment 1" },
+      { id: 2, title: "Assignment 2" }
+    ]);
+  });
 
   PathParameters(app);
   QueryParameters(app);
