b11a9cecae6f96bf9bd2f97c5a5d9e1c701c6b0e
mimahoney <mahoney.mich@northeastern.edu>
2025-04-01 20:49:02 -0400

    Update .env and index.js with new CORS and session configuration3323333322

--- index.js
+++ index.js
@@ -27,24 +27,28 @@ import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
+import session from "express-session";
 const app = express();
-if (process.env.NODE_ENV === "production") {
-    app.set("trust proxy", 1);
-  }
-app.use(cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5173"
-  }));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "kambaz", resave: false,
-    saveUninitialized: false, cookie: {
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "none"
-      }
-      })
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
 );
-
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+    saveUninitialized: false, 
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 // app.use(cors({
 //   origin: "https://a5michaelm.netlify.app", 
