fcac3d6e0cb0d302164f1176de43b2f0e1ba142b
mimahoney <mahoney.mich@northeastern.edu>
2025-04-15 14:20:56 -0400

    a6 updates

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,3 +1,28 @@
+import model from "./model.js";
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
+}
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course, _id: `${user}-${course}` });
+}
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
+}
+export function enrollUserInCourse(user, course) {
+  const newEnrollment = { user, course, _id: `${user}-${course}` };
+  return model.create(newEnrollment);
+ }
+ export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+ }
+ 
+
+
 // import Database from "../Database/index.js";
 // import { v4 as uuidv4 } from "uuid";
 
@@ -8,40 +33,40 @@
 // }
 
 
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+// import Database from "../Database/index.js";
+// import { v4 as uuidv4 } from "uuid";
 
-export function findAllEnrollments() {
-  return Database.enrollments;
-}
-export function enrollUserInCourse(userId, courseId) {
-  const newEnrollment = { _id: uuidv4(), user: userId, course: courseId,
-  };
-Database.enrollments = [...Database.enrollments, newEnrollment];
-  return newEnrollment;
-}
-export function unenrollUserFromCourse(userId, courseId) {
-  const { enrollments } = Database;
-  Database.enrollments = enrollments.filter( (e) => e.user !== userId || e.course !== courseId
-);
-}
+// export function findAllEnrollments() {
+//   return Database.enrollments;
+// }
+// export function enrollUserInCourse(userId, courseId) {
+//   const newEnrollment = { _id: uuidv4(), user: userId, course: courseId,
+//   };
+// Database.enrollments = [...Database.enrollments, newEnrollment];
+//   return newEnrollment;
+// }
+// export function unenrollUserFromCourse(userId, courseId) {
+//   const { enrollments } = Database;
+//   Database.enrollments = enrollments.filter( (e) => e.user !== userId || e.course !== courseId
+// );
+// }
 
-export function findCoursesForUser(userId) {
-  const { courses, enrollments } = Database;
-  return courses.filter((course) =>
-    enrollments.some(
-    (enrollment) => enrollment.user === userId && enrollment.course === course._id)
-);
-}
+// export function findCoursesForUser(userId) {
+//   const { courses, enrollments } = Database;
+//   return courses.filter((course) =>
+//     enrollments.some(
+//     (enrollment) => enrollment.user === userId && enrollment.course === course._id)
+// );
+// }
 
-export function findUsersForCourse(courseId) {
-  const { users, enrollments } = Database;
-  return users.filter((user) =>
-    enrollments.some(
-(enrollment) => enrollment.user === user._id && enrollment.course === courseId
-    )
-  );
-}
+// export function findUsersForCourse(courseId) {
+//   const { users, enrollments } = Database;
+//   return users.filter((user) =>
+//     enrollments.some(
+// (enrollment) => enrollment.user === user._id && enrollment.course === courseId
+//     )
+//   );
+// }
 
 
 // import Database from "../Database/index.js";
